65bdc4018d90978233b74f40da45bdae2ad7afe1
Les Aker <me@lesaker.org>
2016-04-24 23:29:06 -0400

    add projection

--- lib/ballista/projection.rb
+++ lib/ballista/projection.rb
@@ -2,5 +2,9 @@ module Ballista
   ##
   # Projection class for creating journals
   class Projection
+    def initialize(params = {})
+      @entries = params[:entries]
+      @months = params[:months]
+    end
   end
 end
